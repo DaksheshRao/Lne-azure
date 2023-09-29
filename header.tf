277ebbcce2d81072045b1a22e69e0166402bc703
daksheshrao <132909597+DaksheshRao@users.noreply.github.com>
2023-09-29 15:51:31 +0530

    Update header.tf

--- header.tf
+++ header.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = 
-  client_id       = 
-  client_secret   = 
-  tenant_id       = 
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
   features {}
 }
